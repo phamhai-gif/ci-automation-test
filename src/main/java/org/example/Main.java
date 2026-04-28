66ab6eaac34d1c1be251ab6bf77351fa6a04187c
phamhai <25023470@vnu.edu.vn>
2026-04-28 20:28:34 +0700

    Cố tình tạo lỗi để trigger CI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("CI/CD Pipeline Optimization Test! lan 2");
+        System.out.println("CI/CD Pipeline Optimization Test");
     }
 }
